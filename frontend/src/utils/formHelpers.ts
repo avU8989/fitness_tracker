1d0a019690894fb9282221d38f3a04946c8c814b
avU8989 <draagoon8888@live.de>
2025-12-31 11:52:53 +0100

    feat(backend): add helper functions

--- frontend/src/utils/formHelpers.ts
+++ frontend/src/utils/formHelpers.ts
@@ -22,12 +22,35 @@ export function validateTrainingPlanData(
   if (!name.trim())
     return { valid: false, message: "Please enter a plan name." };
 
+  const hasAtLeastOneSplit = days.some(
+    (day) => day.splitType && day.splitType.trim().length > 0
+  );
+
+  if (!hasAtLeastOneSplit) {
+    return {
+      valid: false,
+      message: "Please add at least one training day with a split type.",
+    };
+  }
+
+  const hasTrainingDay = days.some((d) => d.exercises.length > 0);
+  if (!hasTrainingDay)
+    return {
+      valid: false,
+      message: "Please add at least one training day with exercises.",
+    };
+
   for (const day of days) {
-    if (!day.splitType.trim())
+    const isTrainingDay = day.exercises.length > 0;
+
+    if (isTrainingDay && !day.splitType.trim()) {
       return {
         valid: false,
-        message: `Split Type is required for ${day.dayOfWeek}`,
+        message: `Split Type is required for ${day.dayOfWeek} because it has exercises.`,
       };
+    }
+
+    if (!isTrainingDay) continue;
 
     for (const ex of day.exercises) {
       if (!ex.name.trim())
@@ -48,25 +71,22 @@ export function validateTrainingPlanData(
         if (!set.reps || isNaN(set.reps) || set.reps <= 0)
           return {
             valid: false,
-            message: `Reps must be a positive number (Set ${i + 1}) on ${
-              day.dayOfWeek
-            } for ${ex.name}`,
+            message: `Reps must be a positive number (Set ${i + 1}) on ${day.dayOfWeek
+              } for ${ex.name}`,
           };
 
         if (set.weight === undefined || isNaN(set.weight) || set.weight < 0)
           return {
             valid: false,
-            message: `Weight must be zero or positive (Set ${i + 1}) on ${
-              day.dayOfWeek
-            } for ${ex.name}`,
+            message: `Weight must be zero or positive (Set ${i + 1}) on ${day.dayOfWeek
+              } for ${ex.name}`,
           };
 
         if (!set.unit || !["kg", "lbs"].includes(set.unit))
           return {
             valid: false,
-            message: `Unit must be "kg" or "lbs" (Set ${i + 1}) on ${
-              day.dayOfWeek
-            } for ${ex.name}`,
+            message: `Unit must be "kg" or "lbs" (Set ${i + 1}) on ${day.dayOfWeek
+              } for ${ex.name}`,
           };
       }
     }
